276c0b4e1f01df07160d1136d667af18e86f5d54
Piusshungu <shungupius@gmail.com>
2022-04-10 16:16:49 +0300

    Changed the status code 201 on account registration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -51,7 +51,7 @@ class AuthController extends Controller
             'token' => $token
         ];
 
-        return response()->json($response, 200);
+        return response()->json($response, 201);
     }
 
     public function login(Request $request)
