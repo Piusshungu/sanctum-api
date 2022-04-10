bc639a9ef2b3b187b38f390378a6f281d39859c8
Piusshungu <shungupius@gmail.com>
2022-04-10 14:05:26 +0300

    Added a method to view all available shops

--- app/Http/Controllers/ShopsController.php
+++ app/Http/Controllers/ShopsController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Shop;
 use Illuminate\Http\Request;
 
 class ShopsController extends Controller
 {
-    //
+    public function index()
+    {
+        return Shop::all();
+    }
 }
