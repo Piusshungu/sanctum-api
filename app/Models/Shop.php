9ebd96dab5056293e0996a8fdecd262087360798
Piusshungu <shungupius@gmail.com>
2022-04-10 14:13:56 +0300

    Added a filter function in the Shops model

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -15,4 +15,18 @@ class Shop extends Model
     protected $guarded = ['id'];
 
     public $incrementing = false;
+
+    public function scopeFilter($query, array $filters)
+    {
+        if(isset($filters['search'])){
+
+            $query->where('shop_name', 'like', '%'. request('search'). '%')
+
+            ->orWhere('location', 'like', '%'. request('search'). '%')
+
+            ->orWhere('email', 'like', '%'. request('search'). '%')
+
+            ->orWhere('phone_number', 'like', '%'. request('search'). '%');
+        }
+    }
 }
