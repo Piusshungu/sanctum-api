bb4f502ef2aabcc385d71a9b5dd067ffe425e379
Piusshungu <shungupius@gmail.com>
2022-04-11 17:30:26 +0300

    Added a function to update/edit shop details

--- app/Http/Controllers/ShopsController.php
+++ app/Http/Controllers/ShopsController.php
@@ -54,4 +54,34 @@ class ShopsController extends Controller
 
         return response()->json($response, 200);
     }
+
+    public function editShop(Request $request, $id)
+    {
+        $request->validate([
+
+            'shop_name' => 'required',
+            'phone_number' => 'required',
+            'email' => 'required|email|unique:shops,email,',
+            'logo' => 'image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+            'location' => 'required',
+            'category' => 'required',
+            'business_licence' => 'required',
+            'password' => 'required|confirmed'
+        ]);
+
+        $shopDetails = $request->all();
+
+        $shop = Shop::find($id);
+
+        $shop->update($shopDetails);
+
+        $response = [
+
+            'shop' => $shopDetails,
+
+            'message' => 'Shop Details has been updated',
+        ];
+
+        return response()->json($response, 200);
+    }
 }
