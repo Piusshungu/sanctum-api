9a411a3e9c1dd6d8ba5481f3564740d8164e13ca
Piusshungu <shungupius@gmail.com>
2022-04-27 17:31:03 +0300

    Added a method to add products to the cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,5 +6,35 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+    public function cartList()
+    {
+        return \Cart::orderBy('created_at')->getContent();
+    }
+
+    public function addToCart(Request $request)
+    {
+        $product = \Cart::add([
+
+            'name' => 'required',
+
+            'price' => 'required',
+
+            'quantity' => 'required',
+
+            'attributes' => array(
+
+                'image' => $request->image
+            )
+
+            ]);
+
+            $response = [
+
+                'product' => $product,
+
+                'message' => 'Product successfully added to the cart',
+            ];
+
+            return response()->json($response, 200);
+    }
 }
