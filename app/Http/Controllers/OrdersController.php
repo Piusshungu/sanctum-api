729f6d93d8148e9303f031106b724f4c97105bdd
Piusshungu <shungupius@gmail.com>
2022-04-27 12:07:49 +0300

    Added a method  for admin to view a list of all orders

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrdersController extends Controller
 {
-    //
+    public function index()
+    {
+        return Order::orderBy('created_at')->filter(request(['search']))
+            
+        ->paginate(10)->withQueryString()->all();
+    }
 }
